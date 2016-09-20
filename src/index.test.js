a6792ae711ced4457eb1c64702890909a6bc00bf
Mike Spencer <mikespencema@gmail.com>
2016-09-20 18:34:27 -0400

    add a couple of tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,12 @@
-import authMiddleware from './index'
+import authMiddleware, {
+  INIT_AUTH,
+  CONFIGURE,
+  TOKEN_RETRIEVED,
+  TOKEN_NOT_FOUND,
+  REMOVE_TOKEN,
+  getAuthUrl,
+  getTokenFromUrl
+} from './index'
 
 describe('authMiddleware', () => {
   it('should be a function', () => {
@@ -6,4 +14,31 @@ describe('authMiddleware', () => {
   })
 })
 
-// TODO: write tests...
+describe('exports', () => {
+  it('should be defined', () => {
+    expect(INIT_AUTH).toBeDefined()
+    expect(CONFIGURE).toBeDefined()
+    expect(TOKEN_RETRIEVED).toBeDefined()
+    expect(TOKEN_NOT_FOUND).toBeDefined()
+    expect(REMOVE_TOKEN).toBeDefined()
+  })
+})
+
+describe('getAuthUrl', () => {
+  it('should generate the correct auth url', () => {
+    const clientId = 'abc123'
+    const redirectUri = 'http://www.meetup.com'
+    const authUrl = 'http://www.example.com'
+    const actualUrl = getAuthUrl({ clientId, redirectUri, authUrl })
+    const expectedUrl = `${authUrl}?response_type=token&client_id=${clientId}&scope=ageless&redirect_uri=${redirectUri}`
+    expect(actualUrl).toEqual(expectedUrl)
+  })
+})
+
+describe('getTokenFromUrl', () => {
+  it('should get the token from a given url', () => {
+    const token = 'my-token-12345'
+    const url = `http://www.example.com#otherParam=true&access_token=${token}&anotherParam=true`
+    expect(getTokenFromUrl(url)).toEqual(token)
+  })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,8 @@ module.exports = {
     ]
   },
 
+  target: 'node',
+
   resolve: {
     extensions: ['', '.js']
   }
